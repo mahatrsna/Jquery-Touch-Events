10179ec1ddd32e3b59748333378f7f7e6be78c94
mahatrsna <mahatrsna@gmail.com>
2017-02-13 14:53:04 -0600

    Create touch.js

---
+++ touch.js
@@ -0,0 +1,14 @@
+$("body").on("touchstart", ".touchable", function() { //make touchable  items fire like a click event
+var d1 = new Date();
+var n1 = d1.getTime();
+setTimeout(function() {
+    $(".touchable").on("touchend", function(event) {
+        var d2 = new Date();
+        var n2 = d2.getTime();
+        if (n2 - n1 <= 300) {
+            $(event.target).trigger("click"); //dont do the action here just call real click handler
+        }
+    });
+}, 50)}).on("click", "#myelement", function() {
+//all the behavior i originally wanted
+});
